45cf8fda6b2e082c40eb0e3af9ac20309bf5b587
Diarmuid O Connor <doconnor@wit.ie>
2020-01-13 14:54:33 +0000

    Initial structure

---
+++ stories/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+
+
+storiesOf("Demo", module).add("default", () => (
+  <H2>Demo</H2> />
+));
