69d055bb21ede4c021b61411d99a0d62428183db
Diarmuid O Connor <doconnor@wit.ie>
2020-02-05 10:13:40 +0000

    Change site header

--- src/index.js
+++ src/index.js
@@ -1,47 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import HomePage from "./pages/HomePage";
 import MoviePage from "./pages/moviePage";
 import FavoriteMoviesPage from "./pages/favoritesMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage'
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
+import SiteHeader from "./components/siteHeader";
 
 const App = () => {
   return (
     <MoviesContextProvider>
-        <BrowserRouter>
-          <div className="jumbotron">
-            <div className="container-fluid">
-              <ul className="navbar-nav text-black">
-                <li className="nav-item">
-                  <Link className="nav-link " to="/">
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link " to="/movies/favorites">
-                    Favorites
-                  </Link>
-                </li>
-              </ul>
-              <Switch>
-                <Route exact path="/reviews/form" component={AddMovieReviewPage} />
-                <Route path="/reviews/:id" component={MovieReviewPage} />
-                <Route
-                  exact
-                  path="/movies/favorites"
-                  component={FavoriteMoviesPage}
-                />
-                <Route path="/movies/:id" component={MoviePage} />
-                <Route path="/" component={HomePage} />
-                <Redirect from="*" to="/" />
-              </Switch>
-            </div>
+      <BrowserRouter>
+        <div className="jumbotron">
+          <SiteHeader />      {/* New Header    */}
+          <div className="container-fluid">
+            <Switch>
+              <Route
+                exact
+                path="/reviews/form"
+                component={AddMovieReviewPage}
+              />
+              <Route path="/reviews/:id" component={MovieReviewPage} />
+              <Route
+                exact
+                path="/movies/favorites"
+                component={FavoriteMoviesPage}
+              />
+              <Route path="/movies/:id" component={MoviePage} />
+              <Route path="/" component={HomePage} />
+              <Redirect from="*" to="/" />
+            </Switch>
           </div>
-        </BrowserRouter>
+        </div>
+      </BrowserRouter>
     </MoviesContextProvider>
   );
 };
