24fd6badde4224c50e800efdfdf46ca4046f119a
Diarmuid O Connor <doconnor@wit.ie>
2020-09-30 14:07:29 +0100

    Add chunk analysis and reduce vendor bundle

--- src/util.js
+++ src/util.js
@@ -1,7 +1,7 @@
-import _ from "lodash";
+import truncate from "lodash/truncate";
 
 export function excerpt(string) {
-  const truncate = _.truncate;
+  // const truncate = truncate;
   return truncate(string, {
     length: 200, // maximum 200 characters
     separator: /,?\.* +/ // separate by spaces, including preceding commas and periods
