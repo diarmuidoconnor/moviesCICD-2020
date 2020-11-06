7adc6e85afc93b3b15a46cad1a956f1b65442fd1
Diarmuid O Connor <doconnor@wit.ie>
2020-11-06 09:01:38 +0000

    Navigation tests using wrong header links

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -35,16 +35,16 @@ describe("Navigation", () => {
       cy.get("h2").contains(movies[1].title);
     });
     it("should allow navigation from site header", () => {
-      cy.get("nav").find("li").eq(2).find("a").click();
+      cy.get("nav").find("li").eq(3).find("a").click();
       cy.url().should("include", `/favorites`);
       cy.get("h2").contains("Favorite Movies");
       cy.get("nav").find("li").eq(1).find("a").click();
       cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Discover Movies");
       cy.get("nav").find("li").eq(2).find("a").click();
       cy.get("nav.navbar-brand").find("a").click();
       cy.url().should("not.include", `/favorites`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Discover Movies");
     });
   });
 
@@ -72,7 +72,7 @@ describe("Navigation", () => {
     beforeEach(() => {
       cy.visit("/");
       cy.get(".card").eq(0).find("button").click();
-      cy.get("nav").find("li").eq(2).find("a").click();
+      cy.get("nav").find("li").eq(3).find("a").click();
     });
     it("should navigate to the movies detail page and change the browser URL", () => {
       cy.get(".card").eq(0).find("img").click();
@@ -89,11 +89,11 @@ describe("Navigation", () => {
       cy.get(".card").eq(1).find("img").click();
       cy.get("svg[data-icon=arrow-circle-left]").click();
       cy.url().should("not.include", `/movies`);
-      cy.get("h2").contains("No. Movies");
+      cy.get("h2").contains("Discover Movies");
     });
     it("should navigate from favorites page to movie details and back", () => {
       cy.get(".card").eq(1).find("button").click();
-      cy.get("nav").find("li").eq(2).find("a").click();
+      cy.get("nav").find("li").eq(3).find("a").click();
       cy.get(".card").eq(0).find("img").click();
       cy.get("svg[data-icon=arrow-circle-left]").click();
       cy.url().should("include", `/favorites`);
